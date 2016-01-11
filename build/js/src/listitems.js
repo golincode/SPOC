98fdee9c1cd7c12d55c04959075ebae22f303a55
Anthony Roberts <aroberts@wearearchitect.com>
2016-01-11 00:25:56 -0800

    chore(listitems): corrects function name

--- build/js/src/listitems.js
+++ build/js/src/listitems.js
@@ -15,7 +15,7 @@ SPOC.SPSite.prototype.list = function(listTitle) {
      */
     methods.query = function(settings) {
         var listUrl = site.url + '/_api/lists/getByTitle%28%27' + listTitle + '%27%29/items';
-        listUrl += settings ? '?' + SPOC.Utils.convertObjToQueryString(settings) : '';
+        listUrl += settings ? '?' + SPOC.Utils.conversion.ObjToQueryString(settings) : '';
 
         return $.ajax({
             type: "GET",
