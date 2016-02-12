b35d73b3c957a216fad7ab2363dc43fc263705a2
Martin <martinpomeroy@outlook.com>
2016-02-12 16:08:21 +0000

    Hack(): Mock ordering

--- public/js/SPOC.js
+++ public/js/SPOC.js
@@ -57,85 +57,6 @@ SPOC.Utils.Conversion.spInternalName = function(string) {
     return str.replace(/ /g, '_x0020_');
 };
 
-SPOC.Mock = {
-    active: false,
-    db: {}
-};
-
-
-// If offline, set spPageContextInfo
-if (!window._spPageContextInfo) {
-
-    var AppWebUrl = SPOC.Utils.Url.AppWebUrl();
-
-    window._spPageContextInfo = {
-        userId: 1,
-        userLoginName: 'test',
-        webAbsoluteUrl: (AppWebUrl ? AppWebUrl : 'local')
-    };
-
-    if (!AppWebUrl) {
-        SPOC.Mock.active = true;
-    }
-}
-
-
-/**
- * Creates dummy text using lorem ipsum
- * @params  int count  number of words
- * @params  bool isDocument add document extension
- * @return  string seperator to use between words
- */
-SPOC.Mock.dummyText = function(count, isDocument, seperator) {
-    var loremIpsumWordBank = new Array("lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipisicing", "elit,", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua.", "enim", "ad", "minim", "veniam,", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo", "consequat.", "duis", "aute", "irure", "dolor", "in", "reprehenderit", "in", "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur.", "excepteur", "sint", "occaecat", "cupidatat", "non", "proident,", "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum.", "sed", "ut", "perspiciatis,", "unde", "omnis", "iste", "natus", "error", "sit", "voluptatem", "accusantium", "doloremque", "laudantium,", "totam", "rem", "aperiam", "eaque", "ipsa,", "quae", "ab", "illo", "inventore", "veritatis", "et", "quasi", "architecto", "beatae", "vitae", "dicta", "sunt,", "explicabo.", "nemo", "enim", "ipsam", "voluptatem,", "quia", "voluptas", "sit,", "aspernatur", "aut", "odit", "aut", "fugit,", "sed", "quia", "consequuntur", "magni", "dolores", "eos,", "qui", "ratione", "voluptatem", "sequi", "nesciunt,", "neque", "porro", "quisquam", "est,", "qui", "dolorem", "ipsum,", "quia", "dolor", "sit,", "amet,", "consectetur,", "adipisci", "velit,", "sed", "quia", "non", "numquam", "eius", "modi", "tempora", "incidunt,", "ut", "labore", "et", "dolore", "magnam", "aliquam", "quaerat", "voluptatem.", "ut", "enim", "ad", "minima", "veniam,", "quis", "nostrum", "exercitationem", "ullam", "corporis", "suscipit", "laboriosam,", "nisi", "ut", "aliquid", "ex", "ea", "commodi", "consequatur?", "quis", "autem", "vel", "eum", "iure", "reprehenderit,", "qui", "in", "ea", "voluptate", "velit", "esse,", "quam", "nihil", "molestiae", "consequatur,", "vel", "illum,", "qui", "dolorem", "eum", "fugiat,", "quo", "voluptas", "nulla", "pariatur?", "at", "vero", "eos", "et", "accusamus", "et", "iusto", "odio", "dignissimos", "ducimus,", "qui", "blanditiis", "praesentium", "voluptatum", "deleniti", "atque", "corrupti,", "quos", "dolores", "et", "quas", "molestias", "excepturi", "sint,", "obcaecati", "cupiditate", "non", "provident,", "similique", "sunt", "in", "culpa,", "qui", "officia", "deserunt", "mollitia", "animi,", "id", "est", "laborum", "et", "dolorum", "fuga.", "harum", "quidem", "rerum", "facilis", "est", "et", "expedita", "distinctio.", "Nam", "libero", "tempore,", "cum", "soluta", "nobis", "est", "eligendi", "optio,", "cumque", "nihil", "impedit,", "quo", "minus", "id,", "quod", "maxime", "placeat,", "facere", "possimus,", "omnis", "voluptas", "assumenda", "est,", "omnis", "dolor", "repellendus.", "temporibus", "autem", "quibusdam", "aut", "officiis", "debitis", "aut", "rerum", "necessitatibus", "saepe", "eveniet,", "ut", "et", "voluptates", "repudiandae", "sint", "molestiae", "non", "recusandae.", "itaque", "earum", "rerum", "hic", "tenetur", "a", "sapiente", "delectus,", "aut", "reiciendis", "voluptatibus", "maiores", "alias", "consequatur", "aut", "perferendis", "doloribus", "asperiores", "repellat"),
-        docTypes = ['docx', 'jpeg', 'pptx', 'xlsx', 'pdf', 'mp4', 'txt'],
-        ext = isDocument ? docTypes[Math.floor(Math.random() * docTypes.length)] : '',
-        finalString = "",
-        ret = "",
-        newTxt = "";
-
-    seperator = seperator ? seperator : ' ';
-
-    for (var i = 0; i < count; i++) {
-        newTxt = loremIpsumWordBank[Math.floor(Math.random() * (loremIpsumWordBank.length - 1))];
-        if (ret.substring(ret.length - 1, ret.length) == "." || ret.substring(ret.length - 1, ret.length) == "?") {
-            newTxt = newTxt.substring(0, 1).toUpperCase() + newTxt.substring(1, newTxt.length);
-        }
-        ret += seperator + newTxt;
-    }
-
-    if (isDocument) {
-        ret = ret.replace(/,/g, '');
-    }
-
-    finalString = ret.substring(1, ret.length - 1) + "." + ext;
-    return finalString.charAt(0).toUpperCase() + finalString.slice(1);
-
-};
-
-/**
- * Use lorumpixel.com to generate an image
- * @params string  width of image. Defaults to 600
- * @params string  height of image. Defaults to 400
- * @return string
- */
-SPOC.Mock.dummyImage = function(width, height, cat) {
-    width = width ? width : 600;
-    height = height ? height : 400;
-    return 'http://lorempixel.com/' + width + '/' + height + '/' + (cat ? cat : '');
-};
-
-
-/**
- * Creates a Mock SharePoint List
- * @params  listName  name of list to create
- * @params  obj Object of columns and values
- * @return void
- */
-SPOC.Mock.createList = function(listName, data) {
-    SPOC.Mock.db[listName.toLowerCase()] = data;
-};
-
 
 SPOC.Utils.Objects = {};
 
@@ -580,6 +501,7 @@ SPOC.Utils.Storage.removeCookies = function(name) {
     SPOC.Utils.Storage.setCookies(name, "", -1);
 };
 
+
 SPOC.Utils.Strings = {};
 
 /**
@@ -786,6 +708,85 @@ SPOC.Utils.Yammer.checkLogin = function(promptLogin) {
         });
     });
 };
+SPOC.Mock = {
+    active: false,
+    db: {}
+};
+
+
+// If offline, set spPageContextInfo
+if (!window._spPageContextInfo) {
+
+    var AppWebUrl = SPOC.Utils.Url.AppWebUrl();
+
+    window._spPageContextInfo = {
+        userId: 1,
+        userLoginName: 'test',
+        webAbsoluteUrl: (AppWebUrl ? AppWebUrl : 'local')
+    };
+
+    if (!AppWebUrl) {
+        SPOC.Mock.active = true;
+    }
+}
+
+
+/**
+ * Creates dummy text using lorem ipsum
+ * @params  int count  number of words
+ * @params  bool isDocument add document extension
+ * @return  string seperator to use between words
+ */
+SPOC.Mock.dummyText = function(count, isDocument, seperator) {
+    var loremIpsumWordBank = new Array("lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipisicing", "elit,", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua.", "enim", "ad", "minim", "veniam,", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo", "consequat.", "duis", "aute", "irure", "dolor", "in", "reprehenderit", "in", "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur.", "excepteur", "sint", "occaecat", "cupidatat", "non", "proident,", "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum.", "sed", "ut", "perspiciatis,", "unde", "omnis", "iste", "natus", "error", "sit", "voluptatem", "accusantium", "doloremque", "laudantium,", "totam", "rem", "aperiam", "eaque", "ipsa,", "quae", "ab", "illo", "inventore", "veritatis", "et", "quasi", "architecto", "beatae", "vitae", "dicta", "sunt,", "explicabo.", "nemo", "enim", "ipsam", "voluptatem,", "quia", "voluptas", "sit,", "aspernatur", "aut", "odit", "aut", "fugit,", "sed", "quia", "consequuntur", "magni", "dolores", "eos,", "qui", "ratione", "voluptatem", "sequi", "nesciunt,", "neque", "porro", "quisquam", "est,", "qui", "dolorem", "ipsum,", "quia", "dolor", "sit,", "amet,", "consectetur,", "adipisci", "velit,", "sed", "quia", "non", "numquam", "eius", "modi", "tempora", "incidunt,", "ut", "labore", "et", "dolore", "magnam", "aliquam", "quaerat", "voluptatem.", "ut", "enim", "ad", "minima", "veniam,", "quis", "nostrum", "exercitationem", "ullam", "corporis", "suscipit", "laboriosam,", "nisi", "ut", "aliquid", "ex", "ea", "commodi", "consequatur?", "quis", "autem", "vel", "eum", "iure", "reprehenderit,", "qui", "in", "ea", "voluptate", "velit", "esse,", "quam", "nihil", "molestiae", "consequatur,", "vel", "illum,", "qui", "dolorem", "eum", "fugiat,", "quo", "voluptas", "nulla", "pariatur?", "at", "vero", "eos", "et", "accusamus", "et", "iusto", "odio", "dignissimos", "ducimus,", "qui", "blanditiis", "praesentium", "voluptatum", "deleniti", "atque", "corrupti,", "quos", "dolores", "et", "quas", "molestias", "excepturi", "sint,", "obcaecati", "cupiditate", "non", "provident,", "similique", "sunt", "in", "culpa,", "qui", "officia", "deserunt", "mollitia", "animi,", "id", "est", "laborum", "et", "dolorum", "fuga.", "harum", "quidem", "rerum", "facilis", "est", "et", "expedita", "distinctio.", "Nam", "libero", "tempore,", "cum", "soluta", "nobis", "est", "eligendi", "optio,", "cumque", "nihil", "impedit,", "quo", "minus", "id,", "quod", "maxime", "placeat,", "facere", "possimus,", "omnis", "voluptas", "assumenda", "est,", "omnis", "dolor", "repellendus.", "temporibus", "autem", "quibusdam", "aut", "officiis", "debitis", "aut", "rerum", "necessitatibus", "saepe", "eveniet,", "ut", "et", "voluptates", "repudiandae", "sint", "molestiae", "non", "recusandae.", "itaque", "earum", "rerum", "hic", "tenetur", "a", "sapiente", "delectus,", "aut", "reiciendis", "voluptatibus", "maiores", "alias", "consequatur", "aut", "perferendis", "doloribus", "asperiores", "repellat"),
+        docTypes = ['docx', 'jpeg', 'pptx', 'xlsx', 'pdf', 'mp4', 'txt'],
+        ext = isDocument ? docTypes[Math.floor(Math.random() * docTypes.length)] : '',
+        finalString = "",
+        ret = "",
+        newTxt = "";
+
+    seperator = seperator ? seperator : ' ';
+
+    for (var i = 0; i < count; i++) {
+        newTxt = loremIpsumWordBank[Math.floor(Math.random() * (loremIpsumWordBank.length - 1))];
+        if (ret.substring(ret.length - 1, ret.length) == "." || ret.substring(ret.length - 1, ret.length) == "?") {
+            newTxt = newTxt.substring(0, 1).toUpperCase() + newTxt.substring(1, newTxt.length);
+        }
+        ret += seperator + newTxt;
+    }
+
+    if (isDocument) {
+        ret = ret.replace(/,/g, '');
+    }
+
+    finalString = ret.substring(1, ret.length - 1) + "." + ext;
+    return finalString.charAt(0).toUpperCase() + finalString.slice(1);
+
+};
+
+/**
+ * Use lorumpixel.com to generate an image
+ * @params string  width of image. Defaults to 600
+ * @params string  height of image. Defaults to 400
+ * @return string
+ */
+SPOC.Mock.dummyImage = function(width, height, cat) {
+    width = width ? width : 600;
+    height = height ? height : 400;
+    return 'http://lorempixel.com/' + width + '/' + height + '/' + (cat ? cat : '');
+};
+
+
+/**
+ * Creates a Mock SharePoint List
+ * @params  listName  name of list to create
+ * @params  obj Object of columns and values
+ * @return void
+ */
+SPOC.Mock.createList = function(listName, data) {
+    SPOC.Mock.db[listName.toLowerCase()] = data;
+};
+
 
 /**
  * Define Sp Site Object constructor
